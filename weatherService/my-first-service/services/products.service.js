5819b0265d93d47d13fae8f052f438dc73dc808a
unknown <alan.peron@etudiant.univ-rennes1.fr>
2021-11-29 11:01:51 +0100

    docker added (I mean, first attempt)

--- weatherService/my-first-service/services/products.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const DbService = require("moleculer-db");
-
-module.exports = {
-	name: "products",
-	mixins: [DbService]
-};
--- weatherService/my-first-service/services/weather.service.js
+++ weatherService/my-first-service/services/weather.service.js
@@ -10,10 +10,7 @@ module.exports = {
 	name: "weather",
 	actions : {
         async get(){
-            //let promise = await Promise.resolve({ 
-            //    answer: http.get("http://api.weatherapi.com/v1/forecast.json?key=1baa4a47b6694dd89d274000212911&q=London&days=1&aqi=no&alerts=no") 
-            //});
-            let promise = axios.get("http://api.weatherapi.com/v1/forecast.json?key=1baa4a47b6694dd89d274000212911&q=Paris&days=7&aqi=no&alerts=no")
+            let promise = axios.get("http://api.weatherapi.com/v1/forecast.json?key=1baa4a47b6694dd89d274000212911&q=London&days=1&aqi=no&alerts=no")
                 .then(response => {
                     console.log(response);
                     return Object.assign({}, response.data);
