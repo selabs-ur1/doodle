453928cce437a486d8f8a42641593821129f4827
Amoussou Komi <komi.amoussou@etudiant.univ-rennes1.fr>
2021-11-29 20:09:38 +0100

    edit ci.yml and moving JavafuzzExemple back to test directory

--- api/src/test/java/fr/istic/tlc/JavafuzzTestExample.java
+++ api/src/test/java/fr/istic/tlc/JavafuzzTestExample.java
@@ -1,63 +1,44 @@
 package fr.istic.tlc;
 
-import com.gitlab.javafuzz.core.AbstractFuzzTarget;
-
-import io.restassured.response.ValidatableResponse;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.Charset;
-import java.time.Duration;
+
+import com.gitlab.javafuzz.core.AbstractFuzzTarget;
 
 
 public class JavafuzzTestExample extends AbstractFuzzTarget{
 
     
-
     public String[] parseData(byte[] data) {
 
         String dataString = new String(data);
 
-        String [] outputs = new String[3];
-        String [] inputs = dataString.split("\\s+");
+        String[] outputs = new String[3];
+        String[] inputs = dataString.split("\\s+");
 
         outputs[0] = (inputs.length > 1) ? inputs[1] : "";
         outputs[1] = (inputs.length > 1) ? inputs[1] : "";
         outputs[2] = (inputs.length > 2) ? inputs[2] : "";
-        
+
         return outputs;
     }
-    
+
     public void testGet(String[] queries) {
 
-
         String link = (queries.length > 1) ? queries[1] : "";
-    
-        // ValidatableResponse response = given().when().get(link)
-        // .then().assertThat().statusCode(200);
 
-        // System.out.println("Get Response : "+response+"\n\n");
 
         try {
-            
-            HttpRequest request = HttpRequest.newBuilder()
-                                .uri(new URI(link))
-                                .GET().build();
 
-            HttpResponse<String> response = HttpClient.newHttpClient()
-                                            .send(request,HttpResponse.BodyHandlers.ofString());
-                    
+            HttpRequest request = HttpRequest.newBuilder().uri(new URI(link)).GET().build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request,
+                    HttpResponse.BodyHandlers.ofString());
+
             assertEquals(response.statusCode(), 200);
 
         } catch (Exception e) {
@@ -69,33 +50,66 @@ public class JavafuzzTestExample extends AbstractFuzzTarget{
 
     public void testPost(String[] queries) {
 
-            String link = (queries.length > 1) ? queries[1] : "";
-            String inputData = (queries.length > 2) ? queries[2] : "";
+        String link = (queries.length > 1) ? queries[1] : "";
+        String inputData = (queries.length > 2) ? queries[2] : "";
 
-            ValidatableResponse response = given().when().post(link+"?="+inputData)
-            .then().assertThat().statusCode(200);
+        try {
+            
+            HttpRequest request = HttpRequest.newBuilder().uri(new URI(link))
+                    .headers("Content-Type", "application/json;charset=UTF-8")
+                    .POST(HttpRequest.BodyPublishers.ofString(inputData)).build();
 
-            System.out.println("Post Response : " + response + "\n\n");
-        
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            assertEquals(response.statusCode(), 201);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void testPut(String[] queries) {
 
-            String link = (queries.length > 1) ? queries[1] : "";
-            String inputData = (queries.length > 2) ? queries[2] : "";
+        String link = (queries.length > 1) ? queries[1] : "";
+        String inputData = (queries.length > 2) ? queries[2] : "";
 
-            ValidatableResponse response = given().when().put(link + "?=" + inputData).then().assertThat().statusCode(200);
-            System.out.println("Put Response : " + response + "\n\n");
-        
+        try {
+
+            HttpRequest request = HttpRequest.newBuilder().uri(new URI(link))
+                    .headers("Content-Type", "application/json;charset=UTF-8")
+                    .PUT(HttpRequest.BodyPublishers.ofString(inputData)).build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            assertEquals(response.statusCode(), 200);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void testDelete(String[] queries) {
 
-            String link = (queries.length > 1) ? queries[1] : "";
+        String link = (queries.length > 1) ? queries[1] : "";
 
-            ValidatableResponse response = given().when().delete(link).then().assertThat().statusCode(200);
-            System.out.println("Delete Response : " + response + "\n\n");
-        
+        try {
+
+            HttpRequest request = HttpRequest.newBuilder().uri(new URI(link)).DELETE().build();
+
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            assertEquals(response.statusCode(), 200);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
     }
 
     public void fuzz(byte[] data) {
@@ -104,12 +118,14 @@ public class JavafuzzTestExample extends AbstractFuzzTarget{
 
         if (queries.length > 0 && queries[0].equals("GET"))
             testGet(queries);
-        else if(queries.length > 0 && queries[0].equals("POST"))
+        else if (queries.length > 0 && queries[0].equals("POST"))
             testPost(queries);
         else if (queries.length > 0 && queries[0].equals("PUT"))
             testPut(queries);
         else if (queries.length > 0 && queries[0].equals("DELETE"))
             testDelete(queries);
     }
+
+    
 
 }
