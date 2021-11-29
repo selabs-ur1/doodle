68de29e8cf233d56ded046851671b22b52c71d06
Amoussou Komi <komi.amoussou@etudiant.univ-rennes1.fr>
2021-11-29 06:33:03 +0100

    end of javafuzz local test

--- api/src/test/java/fr/istic/tlc/JavafuzzTestExample.java
+++ api/src/test/java/fr/istic/tlc/JavafuzzTestExample.java
@@ -6,11 +6,19 @@ import io.restassured.response.ValidatableResponse;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
+import java.time.Duration;
 
 
 public class JavafuzzTestExample extends AbstractFuzzTarget{
@@ -33,29 +41,34 @@ public class JavafuzzTestExample extends AbstractFuzzTarget{
     
     public void testGet(String[] queries) {
 
-        // if(queries.length > 0 && queries[0].equals("GET")) {
 
-            String link = (queries.length > 1) ? queries[1] : "";
+        String link = (queries.length > 1) ? queries[1] : "";
     
-            ValidatableResponse response = given().when().get(link)
-            .then().assertThat().statusCode(200);
+        // ValidatableResponse response = given().when().get(link)
+        // .then().assertThat().statusCode(200);
 
-            System.out.println("Get Response : "+response+"\n\n");
+        // System.out.println("Get Response : "+response+"\n\n");
 
-        // } else {
+        try {
+            
+            HttpRequest request = HttpRequest.newBuilder()
+                                .uri(new URI(link))
+                                .GET().build();
 
-        //     String q = "";
-        //     for (int i = 0; i < 3; i++)
-        //         q += (queries.length > i) ? queries[i]+" " : "";
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                                            .send(request,HttpResponse.BodyHandlers.ofString());
+                    
+            assertEquals(response.statusCode(), 200);
 
-        //     System.err.println("query : "+ q + "doesn't match GET queries");
-        // }
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
 
     }
 
     public void testPost(String[] queries) {
 
-        // if(queries.length > 0 && queries[0].equals("POST")) {
             String link = (queries.length > 1) ? queries[1] : "";
             String inputData = (queries.length > 2) ? queries[2] : "";
 
@@ -63,50 +76,26 @@ public class JavafuzzTestExample extends AbstractFuzzTarget{
             .then().assertThat().statusCode(200);
 
             System.out.println("Post Response : " + response + "\n\n");
-        // } else {
-
-        //     String q = "";
-        //     for (int i = 0; i < 3; i++)
-        //         q += (queries.length > i) ? queries[i]+" " : "";
-
-        //     System.err.println("query : " + q + "doesn't match POST queries");
-        // }
+        
     }
 
     public void testPut(String[] queries) {
 
-        // if (queries.length > 0 && queries[0].equals("PUT")) {
             String link = (queries.length > 1) ? queries[1] : "";
             String inputData = (queries.length > 2) ? queries[2] : "";
 
             ValidatableResponse response = given().when().put(link + "?=" + inputData).then().assertThat().statusCode(200);
             System.out.println("Put Response : " + response + "\n\n");
-        // } else {
-
-        //     String q = "";
-        //     for (int i = 0; i < 3; i++)
-        //         q += (queries.length > i) ? queries[i] + " " : "";
-
-        //     System.err.println("query : " + q + "doesn't match PUT queries");
-        // }
+        
     }
 
     public void testDelete(String[] queries) {
 
-        // if (queries.length > 0 && queries[0].equals("DELETE")) {
             String link = (queries.length > 1) ? queries[1] : "";
-            //String inputData = (queries.length > 2) ? queries[2] : "";
 
             ValidatableResponse response = given().when().delete(link).then().assertThat().statusCode(200);
             System.out.println("Delete Response : " + response + "\n\n");
-        // } else {
-
-        //     String q ="";
-        //     for(int i=0; i<3; i++)
-        //         q += (queries.length > i) ? queries[i] + " " : "";
-
-        //     System.err.println("query : " + q + "doesn't match DELETE queries");
-        // }
+        
     }
 
     public void fuzz(byte[] data) {
