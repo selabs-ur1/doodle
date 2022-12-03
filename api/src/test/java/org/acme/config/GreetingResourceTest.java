6ca036282c424cf019eca5a06c7a66858f844885
ewenbrune <102039819+ewenbrune@users.noreply.github.com>
2022-12-03 14:13:10 +0100

    Update GreetingResourceTest.java

--- api/src/test/java/org/acme/config/GreetingResourceTest.java
+++ api/src/test/java/org/acme/config/GreetingResourceTest.java
@@ -10,13 +10,5 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        /*given()
-                .when().get("/config-yaml/greeting")
-                .then()
-                .statusCode(200)
-                .body(is("hello quarkus!"));*/
-    }
-
+   
 }
